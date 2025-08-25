d752151cb05cdb5674977a487db0f9a1ec22fc6f
sriramsetty gayathri <23jr1a05e8@gmail.com>
2025-08-25 15:07:32 +0530

    updating api for ai

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request
+from main import generateAI
+import pickle
+
+generateAI()
+ai=pickle.load(open('model.pkl','rb'))
+
+app=Flask(__name__)
+@app.route('/')
+def home():
+    return('AI Model Server is running')
+
+@app.route(('/predict',methods=['GET']))
+def predict():
+    temp = request.args.get('temp')
+    temp=float(temp)
+    data=[[temp]]
+    result=ai.predict(data)
+    result=result[0]
+    return(result)
+
+if(__name__=="__main__"):
+    app.run(host='0.0.0.0',port=5000,debug=True)
---
+++ main.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+def generateAI():
+    dataset = pd.read_csv('data.csv')
+    dataset = dataset.dropna()
+    X=X.reshape(-1,1)
+    y=dataset.iloc[:,1].values
+    from sklearn.model_selection import train_test_split
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+    from sklearn.neighbors import KNeighborsClassifier
+    ai=KNeighborsClassifier(n_neighbors=5)
+    ai.fit(X_train,y_train)
+    import pickle
+    pickle.dump(ai,open('model.pickle','wb'))
